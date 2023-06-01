da021496788a50280d70be711e2b185d97979f71
Gizela Delgado Soto <74159414+gizelads@users.noreply.github.com>
2023-05-31 20:58:56 -0400

    Add request to R&M api using feign client

--- src/main/java/rm/api/search/cli/api/ApiRMFeignClient.java
+++ src/main/java/rm/api/search/cli/api/ApiRMFeignClient.java
@@ -1,4 +1,14 @@
 package rm.api.search.cli.api;
 
+import feign.Headers;
+import feign.QueryMap;
+import feign.RequestLine;
+import rm.api.search.cli.api.dto.CharacterListApiResponse;
+
+import java.util.Map;
+
+@Headers("Accept: application/json")
 public interface ApiRMFeignClient {
+    @RequestLine("GET /api/character")
+    CharacterListApiResponse filterCharacters(@QueryMap Map<String, Object> queryMap);
 }
