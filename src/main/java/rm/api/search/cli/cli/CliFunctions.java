0aafe453262de79208f3bd76b9e5e75e5ab7dbb2
Gizela Delgado Soto <74159414+gizelads@users.noreply.github.com>
2023-05-31 21:42:29 -0400

    Add toMap cli function

--- src/main/java/rm/api/search/cli/cli/CliFunctions.java
+++ src/main/java/rm/api/search/cli/cli/CliFunctions.java
@@ -1,4 +1,17 @@
 package rm.api.search.cli.cli;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface CliFunctions {
+    static Map<String, Object> toMap(CliArguments cliArguments) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", cliArguments.getName());
+        params.put("status", cliArguments.getStatus());
+        params.put("species", cliArguments.getSpecies());
+        params.put("type", cliArguments.getType());
+        params.put("gender", cliArguments.getGender());
+
+        return params;
+    }
 }
